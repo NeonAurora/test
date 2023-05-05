32fa7e2575e0caf54015a2fa23ae1b548d48da76
Arn <arnarnsde@gmail.com>
2023-05-05 20:22:29 +0600

    resolve #3

--- script.js
+++ script.js
@@ -2,17 +2,19 @@ const button = document.getElementById("myButton");
 
 let timeoutId;
 
-button.addEventListener("mousedown", function() {
-  timeoutId = setTimeout(function() {
+button.addEventListener("mousedown", function () {
+  timeoutId = setTimeout(function () {
     button.classList.add("long-press");
+
+    alert("Long click detected!");
   }, 1000); // wait for 1 second to detect long press
 });
 
-button.addEventListener("mouseup", function() {
+button.addEventListener("mouseup", function () {
   clearTimeout(timeoutId);
   button.classList.remove("long-press");
 });
 
-button.addEventListener("click", function() {
+button.addEventListener("click", function () {
   alert("Hello, world!");
 });
